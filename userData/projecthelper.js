643d5bf486a4c4f2954485fec0c8c9f4a9411578
BerniceMeghan <bernicemeghan@gmail.com>
2023-02-09 20:19:15 -0500

    first commit

---
+++ userData/projecthelper.js
@@ -0,0 +1,28 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Question Pper Generator",
+    desc: "My final semester project was to design a website for our college St francis college for women to conduct exams at ease",
+    image: "/images/projects/questionpaper.png",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+  {
+    id: 2,
+    name: "Yelp Camp",
+    desc: "A Project based on MERN tech stack Ejs instead of React with SDks like mapbox gl and Cloudinary. One of the top full stack Web development project ever Made !!",
+    image: "/images/projects/yelpcamp.png",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+  
+ 
+  
+ 
+  
+  
+];
